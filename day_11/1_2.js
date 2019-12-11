b2cec9c7bee474a5d4efc0b14e7fb66c7e3b709e
Janis Locans <jlocans@fia-tech.com>
2019-12-11 14:13:37 +0200

    Day 11 Part 1.

---
+++ day_11/1_2.js
@@ -0,0 +1,31 @@
+const puzzleInput = require('./input');
+const IntCodeComputer = require('../intCodeComputer');
+const Robot = require('../robot');
+
+console.log(solve(puzzleInput));
+
+function solve(input) {
+    const computer = new IntCodeComputer(input);
+    const robot = new Robot(getEstimatedGridSize(input));
+
+    while (!computer.mustHalt) {
+        const output = computer.executeInstruction(robot.color);
+
+        if (output !== null)
+            robot.processInput(output);
+    }
+
+    robot.grid.forEach(r => {
+        console.log(r.map(c => c ? '#' : ' ').join(''));
+    });
+
+    return robot.paintedPanels.size;
+}
+
+function getEstimatedGridSize(input) {
+    const rgx = /,(?:4|104),/gi;
+    const moveCommandsCount = input.match(rgx).length / 2;
+    const maxDistance = Math.floor((moveCommandsCount + 1) / 2);
+
+    return maxDistance * 8 + 1; // trial and error tbh
+}
---
+++ robot.js
@@ -0,0 +1,70 @@
+class Robot {
+    constructor(gridSize) {
+        this.movesCount = 0;
+        this.paintedPanels = new Set();
+        this.grid = initGrid(gridSize);
+        this.x = Math.floor(gridSize / 2);
+        this.y = Math.floor(gridSize / 2);
+        this.directionAngle = 90;
+        this.color = 1;
+        this.waitingColor = true;
+
+        console.log('Initiating robot...');
+        console.log('gridSize', gridSize);
+        console.log('x', this.x);
+        console.log('y', this.y);
+
+        this.processInput = function (input) {
+            if (this.waitingColor) {
+                this.color = input;
+            }
+            else {
+                this.move(input);
+            }
+        }
+
+        this.move = function (turnVal) {
+            this.directionAngle = ((turnVal === 0 ? this.directionAngle + 90 : this.directionAngle - 90) + 360) % 360;
+
+            switch (this.directionAngle) {
+                case 0:
+                    this.x++;
+                    break;
+                case 90:
+                    this.y--;
+                    break;
+                case 180:
+                    this.x--;
+                    break;
+                case 270:
+                    this.y++;
+                    break;
+                default: throw ('Incorrect angle');
+            }
+
+            this.waitingColor = true;
+        }
+    }
+
+    get color() {
+        return this.grid[this.x][this.y];
+    }
+
+    set color(color) {
+        if (this.color !== color) {
+            this.paintedPanels.add(`${this.x},${this.y}`);
+        }
+
+        // console.log(`Changing color from ${this.color} to ${color} at ${this.x},${this.y}`);
+        this.grid[this.x][this.y] = color;
+        this.waitingColor = false;
+    }
+}
+
+function initGrid(size) {
+    return Array(size)
+        .fill(null)
+        .map(_ => Array(size).fill(0));
+}
+
+module.exports = Robot;
